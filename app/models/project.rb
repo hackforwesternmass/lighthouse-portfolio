bd327ba606ae25b142a79fd23cec3a8623c86338
mtuck063 <meneliktucker@gmail.com>
2015-11-17 03:45:59 -0500

    fixed priority form not working properly

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,15 +6,15 @@ class Project < ActiveRecord::Base
   has_attached_file :photo, :default_url => "blue-space-cloud.jpg"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
- scope :prioritize, -> { order(priority: :asc, created_at: :desc) } 
+ scope :prioritize, -> { order(priority: :desc, created_at: :desc) } 
 
   def next
-  	nex = Project.where('id > ?', self.id).first
+  	nex = Project.prioritize.where('id > ?', self.id).first
   	if nex.nil? then Project.first else nex end
   end
 
   def previous
-  	prev = Project.where('id < ?', self.id).last
+  	prev = Project.prioritize.where('id < ?', self.id).last
   	if prev.nil? then Project.last else prev end
   end
 
