1a9800ad787c3e2c4f6bd8a6d9889504dc9269fb
Andrew Pasquale <andrew@elytra.net>
2016-08-21 16:55:05 -0400

    Add Mon/Wed and Tu/Th class options and switch Seasons to Blocks 1-5.

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -3,8 +3,8 @@ class Klass < ActiveRecord::Base
   pg_search_scope :default_search, :against => [:name, :description, :instructor, :weekday, :time, :year, :season], :using => {:tsearch => {:prefix => true}}
   default_scope { order(created_at: :desc) }
 
-  VALID_WEEKDAYS_TYPES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-  VALID_SEASON_TYPES = ["Fall", "Winter"]
+  VALID_WEEKDAYS_TYPES = ["Monday", "Monday/Wednesday", "Tuesday", "Tuesday/Thursday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  VALID_SEASON_TYPES = ["Block 1", "Block 2", "Block 3", "Block 4", "Block 5"]
 
   has_many :enrolls, dependent: :destroy
   has_many :users, through: :enrolls
