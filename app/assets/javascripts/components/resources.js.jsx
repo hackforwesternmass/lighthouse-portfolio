dccb86a8cc25148aa122c242bd99be392a87dba4
Menelik Tucker <meneliktucker@gmail.com>
2017-07-16 09:53:24 -0400

    text over flow bread crumb

--- app/assets/javascripts/components/resources.js.jsx
+++ app/assets/javascripts/components/resources.js.jsx
@@ -5,14 +5,8 @@ const Resources = React.createClass({
   componentDidMount() {
     this.updateResourceGroup()
     $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
+      constrainWidth: false,
+      belowOrigin: true,
     });
   },
   toggleEdit(e) {
