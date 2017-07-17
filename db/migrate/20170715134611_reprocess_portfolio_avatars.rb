38508f114596c071b0773c1dfcf3361a2847bad7
Menelik Tucker <meneliktucker@gmail.com>
2017-07-17 08:15:57 -0400

    Add validator to portfolio

--- db/migrate/20170715134611_reprocess_portfolio_avatars.rb
+++ db/migrate/20170715134611_reprocess_portfolio_avatars.rb
@@ -3,6 +3,7 @@ class ReprocessPortfolioAvatars < ActiveRecord::Migration
     has_attached_file :avatar,
                       default_url: -> (avatar) { ActionController::Base.helpers.asset_url('default-avatar.png') },
                       styles: { thumb: "100x100#", medium: '250x250#' }
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   end
 
   def up
